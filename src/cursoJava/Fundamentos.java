71748c78b1ab946a9e046757800dde04f7b190a3
IgberZBN <igber_benitez@outlook.com>
2023-02-13 18:24:12 -0300

    Operadores aritmeticos

---
+++ src/cursoJava/Fundamentos.java
@@ -0,0 +1,24 @@
+package cursoJava;
+
+public class Fundamentos {
+
+	public static void main(String[] args) {
+//		String nome = "Gabriel Benitez";
+		
+		String nome;
+		int idade = 18;
+		char sexo = 'M';
+		double temperatura = 26.7;
+		boolean arCondicionado = false;
+				
+		nome = "Gabriel Benitez";
+		
+		System.out.println(nome);
+		System.out.println(idade);
+		System.out.println(sexo);
+		System.out.println(temperatura);
+		System.out.println(arCondicionado);
+		
+	}
+
+}
