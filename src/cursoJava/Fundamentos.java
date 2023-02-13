bb53163bbe737cdc9fc38e2f80aa274747ea60c4
IgberZBN <igber_benitez@outlook.com>
2023-02-13 19:09:05 -0300

    Estrutura de controle condicionais

--- src/cursoJava/Fundamentos.java
+++ src/cursoJava/Fundamentos.java
@@ -18,10 +18,8 @@ public class Fundamentos {
 		System.out.println(sexo);
 		System.out.println(temperatura);
 		System.out.println(arCondicionado);
-		
 		System.out.println("---------------------");
 		System.out.println("");
-		
 		double i = 10;
 		System.out.println("Operadores Aritmeticos e Atribuicoes");
 		System.out.println("Exemplos:");
@@ -35,6 +33,48 @@ public class Fundamentos {
 		System.out.println("i -=         | i = " + (i -= 5));
 		System.out.println("i *=         | i = " + (i *= 5));
 		System.out.println("i /=         | i = " + (i /= 5));
+		System.out.println("---------------------");
+		System.out.println("");
+		System.out.println("Estruturas de controle condicional");
+		System.out.println("");
+		System.out.println("Exemplo 1- Uso do 'if'");
+		if( sexo == 'M') {
+			System.out.println("Aliastamento militar obrigatorio");
+		}
+		System.out.println("Exemplo 2 - Uso do 'if - else'");
+		if(idade < 18) {
+			System.out.println("Menor de idade");
+		} else {
+			System.out.println("Maior de idade");
+		}
+		System.out.println("");
+		System.out.println("Exemplo 3 - Uso do 'else -if'");
+		if(idade < 16) {
+			System.out.println("PROIBIDO VOTAR");
+		} else if (idade >= 18 && idade <= 70) {
+			System.out.println("VOTO OBRIGATORIO");
+		} else if (idade == 16 || idade == 17 || idade > 70) {
+			System.out.println("VOTO FACULTATIVO");
+		}
+		System.out.println("Exemplo 4 - Uso do 'switch case'");
+		System.out.println("1. Cadastro de clientes");
+		System.out.println("2. Cadastro de usuarios");
+		System.out.println("3. Relatorios");
+		int opcao = 4;
+		switch (opcao) {
+		case 1:
+			System.out.println("CLIENTES");
+			break;
+		case 2:
+			System.out.println("USUARIOS");
+			break;
+		case 3:
+			System.out.println("RELATORIOS");
+			break;
+		default:
+			System.out.println("Opcao invalida");
+		}
+
 	}
 
 }
