b761a9b9599a6794fa26abf056b0af10cb2a5803
IgberZBN <igber_benitez@outlook.com>
2023-02-13 19:26:43 -0300

    Estrutura de controle condicionais

--- src/cursoJava/Fundamentos.java
+++ src/cursoJava/Fundamentos.java
@@ -74,7 +74,37 @@ public class Fundamentos {
 		default:
 			System.out.println("Opcao invalida");
 		}
-
+		System.out.println("---------------------");
+		System.out.println("");
+		System.out.println("Estruturas de repeticoes");
+		System.out.println("");
+		System.out.println("Exemplo 5 - Uso do 'for");
+		for (int j = 10; j > 0; j--) {
+			System.out.print(j+" ");
+		}
+		System.out.println("");
+		System.out.println("Exemplo 6 - Tabuada");
+		for(int tabuada = 0; tabuada <= 10; tabuada++) {
+			System.out.println("");
+			for (int valor = 0; valor <= 10; valor++) {
+				System.out.println(tabuada + " x " + valor + " = " + (tabuada*valor));
+			}
+		}
+		System.out.println("");
+		System.out.println("Exemplo 7 - Uso da estrutura while");
+		int contador = 1;
+		while (contador <= 10) {
+			System.out.println(contador);
+			contador++;
+		}
+		System.out.println("");
+		System.out.println("Exemplo 8 - Uso da estrutura do while");
+		char novoJogo;
+		do {
+			System.out.println("Deseja jogar novamente(s/n)?");
+			novoJogo = 'n'
+		} while (novoJogo == 's');
+		System.out.println("GAME OVER");
 	}
 
 }
