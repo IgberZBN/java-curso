3d9d8810402b364cdf147f8108cde0b950695530
IgberZBN <igber_benitez@outlook.com>
2023-02-13 16:54:49 -0300

    Inicio do curso

---
+++ src/cursoJava/Hello.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package cursoJava;
+
+/**
+ * @author igber
+ * Hello world
+ */
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
